0dbb78e4f95a859266b942b57b2244ec3b65fb79
PUGE <mail@puge.org>
2024-08-09 22:49:38 +1000

    save

--- app.py
+++ app.py
@@ -8,16 +8,21 @@ def sanitize_filename(filename):
     
     # 使用正则表达式替换这些字符为空字符串
     sanitized_filename = re.sub(forbidden_chars, '', filename)
-    
+    sanitized_filename = sanitized_filename.replace('\\r', '')
+    sanitized_filename = sanitized_filename.replace('\\n', '')
+
     return sanitized_filename
 
 def create_directory_structure(base_path, bookmarks):
     for bookmark in bookmarks:
         path = os.path.join(base_path, *bookmark['titles'])
+        path = sanitize_filename(path)
+        path = path.replace('\r', '')
+        path = path.replace('\n', '')
+        path = path.strip()
         os.makedirs(path, exist_ok=True)
 
 def extract_bookmarks(pdf_reader):
-    
     def _extract(bookmarks, titles=[], ind=0):
         extracted = []
         for bookmark in bookmarks:
@@ -35,7 +40,7 @@ def split_pdf_by_bookmarks(pdf_reader, output_dir, bookmarks):
     for i, bookmark in enumerate(bookmarks):
         output_pdf = PdfWriter()
         start_page = bookmark['page']  # Page numbers are 1-based in bookmarks, 0-based in PyPDF2
-        if (start_page):
+        if (start_page != None):
             if (i + 1) < len(bookmarks) and 'page' in bookmarks[i + 1] and bookmarks[i + 1]['page']:
                 end_page = bookmarks[i + 1]['page']
             else:
